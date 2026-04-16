052c46ff0d70393fc34ec2156b266532b2744034
Santiago Simon <santiago.simon.longo@gmail.com>
2026-04-16 11:42:01 -0300

    Actualizo mi repo para que sea mas organizado

---
+++ guia FUNCIONES/ej_5.c
@@ -0,0 +1,20 @@
+/*
+5) Escribir una función que dados a y b ambos de tipo entero devuelva a ^ b de
+tipo double para los siguientes casos (no se puede utilizar ninguna biblioteca
+de funciones matemáticas). Implementar una solución iterativa.
+En las pruebas debe invocar a la función para los siguientes 5 casos:
+
+i) a > 0 y b > 0
+ii) a > 0 y b < 0
+iii) a < 0 y b > 0
+iv) a < 0 y b < 0
+v) a > 0 y b = 0
+
+Ejemplos:
+i) a = 2 ; b = 3 ; resultado = 8
+ii) a = -2 ; b = 3 ; resultado = 8
+iii) a = -2 ; b = 0 ; resultado = 1
+iv) a = -2 ; b = 0 ; resultado = 1
+*/
+
+#include <stdio.h>
